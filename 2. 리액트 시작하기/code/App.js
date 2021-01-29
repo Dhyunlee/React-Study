5279f305961b01f437788ca0e0235d9f30dc163b
leedong2897 <skyblue5030@naver.com>
2021-01-29 14:50:27 +0900

    react 공부 정리

---
+++ 2. 리액트 시작하기/code/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>안녕하세요!!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
